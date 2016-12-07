080959fd645879638c9623a6bf9ae7a8241ab742
Jhonny Bill Mena <JhonnyBill@Jhonny-Bills-MacBook-Pro.local>
2016-12-07 19:39:20 -0400

    Created xCode project for FitMap

---
+++ FitMap/FitMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitMap
+//
+//  Created by Jhonny Bill Mena on 12/7/16.
+//  Copyright © 2016 FormuladoresDiscretos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

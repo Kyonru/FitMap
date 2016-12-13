2e31d99259b56b4c30fe102057686a649b76adb3
fitmap <fitmap@dkt6bbf8j4.lab.intec.local>
2016-12-13 18:57:47 -0400

    Creacion de Agregar Ruta Storyboard y la creacion del viewcontroller de agregar ruta

---
+++ FitMap/FitMap/AgregarRuta.swift
@@ -0,0 +1,13 @@
+//
+//  AgregarRuta.swift
+//  FitMap
+//
+//  Created by fitmap on 12/13/16.
+//  Copyright © 2016 FormuladoresDiscretos. All rights reserved.
+//
+
+import UIKit
+
+class AgregarRuta: NSObject {
+
+}

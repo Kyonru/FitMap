d943b0bd415cbf39c4d5c6cb191cebccc6749671
Lareen Melo <zelo@Lareens-MacBook-Pro.local>
2016-12-20 18:55:53 -0400

    Se actualizaron las vistas para que funcionen correctamente y realizar los storyboards y que se vean las vistas

---
+++ FitMap/FitMap/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  FitMap
+//
+//  Created by Lareen Melo on 12/20/16.
+//  Copyright © 2016 FormuladoresDiscretos. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: NSObject {
+
+}

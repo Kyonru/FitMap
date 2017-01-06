d5bb4eb7878f8f517ffe06b0bd0514eefb93773d
jhonnybillm@gmail.com <jhonnybillm@gmail.com>
2017-01-06 12:52:00 -0400

    added tracking capabilities to map. Note: button disappears when map is up

--- FitMap/FitMap/AgregarRutaViewController.swift
+++ FitMap/FitMap/AgregarRutaViewController.swift
@@ -7,15 +7,64 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
-class AgregarRutaViewController: UIViewController {
 
+class AgregarRutaViewController: UIViewController, CLLocationManagerDelegate{
+
+    var locationManager = CLLocationManager()
+    let path = GMSMutablePath()
+    
+    
+    @IBOutlet weak var MapView: GMSMapView!
+    
+    var rectangle = GMSPolyline()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager = CLLocationManager()
+            self.locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
+        MapView.isMyLocationEnabled = true
+
+    
+
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //Locaciones.append(locations.last!)
+        let locValue = locations.last!
+        
+        let long = locValue.coordinate.longitude
+        let lat = locValue.coordinate.latitude
+        let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 15.0)
+        MapView.camera = camera
+        
+        MapView.isMyLocationEnabled = true
+        
+        
+        view = MapView
+        
+        path.add(locValue.coordinate)
+        rectangle = GMSPolyline(path: path)
+        rectangle.map = MapView
+        //locationManager.stopUpdatingLocation()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

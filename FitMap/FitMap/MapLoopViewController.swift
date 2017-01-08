b442fe855bf35beb2fb5d9dfb5b1cb3306e35b2b
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-01-08 16:28:07 -0400

    Added tracking capabilities to LoopView.

---
+++ FitMap/FitMap/MapLoopViewController.swift
@@ -0,0 +1,75 @@
+//
+//  MapLoopViewController.swift
+//  FitMap
+//
+//  Created by Jhonny Bill Mena on 1/8/17.
+//  Copyright © 2017 FormuladoresDiscretos. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import CoreLocation
+
+class MapLoopViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    let path = GMSMutablePath()
+    
+    @IBOutlet weak var MapView: GMSMapView!
+    
+    var rectangle = GMSPolyline()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+  
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.locationServicesEnabled()){
+            locationManager = CLLocationManager()
+            self.locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+    }
+}
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //Locaciones.append(locations.last!)
+        let locValue = locations.last!
+        
+        let long = locValue.coordinate.longitude
+        let lat = locValue.coordinate.latitude
+        let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: 15.0)
+        MapView.camera = camera
+        
+        MapView.isMyLocationEnabled = true
+        
+        
+        //view = MapView
+        
+        path.add(locValue.coordinate)
+        rectangle = GMSPolyline(path: path)
+        rectangle.map = MapView
+        //locationManager.stopUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

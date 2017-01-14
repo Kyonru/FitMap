c24ea5874f9ec1128b711cc8ab99a8dba9318a8d
jhonnybillm@gmail.com <jhonnybillm@gmail.com>
2017-01-13 20:03:27 -0400

    Added how to save a route form. Added a few models / viewController methods

---
+++ FitMap/FitMap/routeSaving.swift
@@ -0,0 +1,24 @@
+//
+//  routeSaving.swift
+//  FitMap
+//
+//  Created by fitmap on 1/13/17.
+//  Copyright © 2017 FormuladoresDiscretos. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+
+class routeSaving {
+    
+    var route = Route()
+    
+    func insertRoute(route: Route) {
+        
+        //Querys etc to insert a route
+        
+        
+    }
+    
+}

0d27c17b8327ed743e352f12239cb32bb8fd0ca7
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-01-22 17:38:00 -0400

    Fu$%# saving userId on iPhone with coreData. F$%#

--- FitMap/FitMap/UserSaving.swift
+++ FitMap/FitMap/UserSaving.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
 
 class UserSaving{
     
@@ -31,6 +33,71 @@ class UserSaving{
     
     //Una vez tengo el id del usuario, guardo ese int en el iphone con CoreData
     
+
+    @available(iOS 10.0, *)
+    func saveUser(id: Int) {
+        
+//        let context = getContext()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        //retrieve the entity that we just created
+        let entity =  NSEntityDescription.entity(forEntityName: "UserData", in: context)
+        
+        let usr = NSManagedObject(entity: entity!, insertInto: context)
+        
+        //set the entity values
+        usr.setValue(id, forKey: "userID")
+//        transc.setValue(audioFileUrlString, forKey: "audioFileUrlString")
+//        transc.setValue(textFileUrlString, forKey: "textFileUrlString")
+        
+        //save the object
+        do {
+            try context.save()
+            print("saved!")
+        } catch let error as NSError  {
+            print("Could not save \(error), \(error.userInfo)")
+        } catch {
+            
+        }
+    }
+    
+    
+    /////////////////////////////////////////////////////////////////////////
+    //                         RETRIEVE USER ID                           //
+    ////////////////////////////////////////////////////////////////////////
+    
+    @available(iOS 10.0, *)
+    func getUserId() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedObjectContext = appDelegate.managedObjectContext
+        
+        // Create Fetch Request
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "UserData")
+        
+        // Helpers
+        var result = NSManagedObject()
+        
+        do {
+            // Execute Fetch Request
+            let records = try managedObjectContext.fetch(fetchRequest)
+            
+            if let records = records as? NSManagedObject {
+                result = records
+            }
+            
+        } catch {
+            print("Unable to fetch managed objects for entity UserData.")
+        }
+        
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        
+        print(result)
+        
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        
+        
+    }
     
     
     
